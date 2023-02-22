30bbb160cdaab4d022cbaa1beb9db342e920c462
richcire <eric9907@naver.com>
2023-02-22 13:13:20 +0900

    Feat: make mobile about sidebar

--- src/components/about/AboutSideBar.tsx
+++ src/components/about/AboutSideBar.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { isMobile } from "react-device-detect";
+import { BiMenu } from "react-icons/bi";
 
 const AboutSideBarContainer = styled.div`
   display: flex;
@@ -15,6 +17,15 @@ const AboutSideBarContainer = styled.div`
   @media screen and (max-width: 320px) {
     width: 25vw;
   }
+`;
+
+const MobileAboutSideBarContainer = styled.div`
+  margin-top: 8vh;
+  display: flex;
+  flex-direction: column;
+  width: 60vw;
+  height: 100%;
+  background-color: #e0e7e9;
 `;
 
 const NestedListHeader = styled.div`
@@ -33,23 +44,47 @@ const MenuLink = styled(Link)`
   margin-left: 1vw;
   font-weight: 800;
   white-space: nowrap;
-  @media screen and (max-width: 768px) {
-    font-size: 0.8rem;
-  }
-  @media screen and (max-width: 490px) {
-    font-size: 5%;
-  }
+`;
+
+const MobileNavigatorBtn = styled.button`
+  position: absolute;
+  top: 10vh;
+  border: none;
+  background-color: transparent;
+`;
+
+interface OverlayType {
+  isOpen: boolean;
+}
+
+const Overlay = styled.div<OverlayType>`
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.6);
+  width: 100vw;
+  height: 100vh;
+  display: ${(props) => (props.isOpen ? "block" : "none")};
 `;
 
 function AboutSideBar() {
   const [open, setOpen] = useState(true);
   const navigate = useNavigate();
+  const [isMobileSideBarOpen, setIsMobileSideBarOpen] = useState(false);
 
   const handleClick = () => {
     setOpen(!open);
   };
 
-  return (
+  const handleMobileNavigatorBtnClick = () => {
+    setIsMobileSideBarOpen(true);
+  };
+
+  const handleOverlayClick = () => {
+    setIsMobileSideBarOpen(false);
+  };
+
+  return !isMobile ? (
     <AboutSideBarContainer>
       <List>
         <ListItemButton
@@ -91,6 +126,55 @@ function AboutSideBar() {
         </Collapse>
       </List>
     </AboutSideBarContainer>
+  ) : (
+    <>
+      <MobileNavigatorBtn onClick={handleMobileNavigatorBtnClick}>
+        <BiMenu size="30px" />
+      </MobileNavigatorBtn>
+      <Overlay isOpen={isMobileSideBarOpen} onClick={handleOverlayClick}>
+        <MobileAboutSideBarContainer>
+          <List>
+            <ListItemButton
+              onClick={() => navigate("/about/main")}
+              sx={{
+                paddingTop: "15px",
+                paddingBottom: "15px",
+                borderBottom: "1px solid",
+              }}
+            >
+              <MenuLink style={{ fontSize: "20px" }} to={"/about/main"}>
+                소개
+              </MenuLink>
+            </ListItemButton>
+            <ListItemButton
+              onClick={handleClick}
+              sx={{
+                paddingTop: "15px",
+                "@media screen and (max-width: 1200px)": { display: "none" },
+              }}
+            >
+              <NestedListHeader>스꾸도 사용방법</NestedListHeader>
+            </ListItemButton>
+            <Collapse in={open}>
+              <List>
+                <ListItemButton onClick={() => navigate("/about/apply")}>
+                  <MenuLink to={"/about/apply"}>동아리 지원</MenuLink>
+                </ListItemButton>
+                <ListItemButton onClick={() => navigate("/about/makeclub")}>
+                  <MenuLink to={"/about/makeclub"}>동아리 생성</MenuLink>
+                </ListItemButton>
+                <ListItemButton onClick={() => navigate("/about/mypage")}>
+                  <MenuLink to={"/about/mypage"}>개인 정보</MenuLink>
+                </ListItemButton>
+                <ListItemButton onClick={() => navigate("/about/manage")}>
+                  <MenuLink to={"/about/manage"}>동아리 관리</MenuLink>
+                </ListItemButton>
+              </List>
+            </Collapse>
+          </List>
+        </MobileAboutSideBarContainer>
+      </Overlay>
+    </>
   );
 }
 
